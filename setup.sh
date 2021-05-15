acc8040495ffb37cd6523b30b91af9324fdd23ca
Gaurav Keswani <gaurav.keswani@shopify.com>
2021-05-15 14:16:45 -0700

    Install ZSH plugins for auto complete and syntax highlighting

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@ ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 
 echo "Installing Oh My ZSH"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Installing basic ZSH plugins"
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
