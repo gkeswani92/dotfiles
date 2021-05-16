83ff9cddd30412b69c45e4a3cd07824738bfbc74
Gaurav Keswani <gaurav.keswani@shopify.com>
2021-05-16 15:15:44 -0700

    Create symlink to .zshrc

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 
 echo "Installing Oh My ZSH"
@@ -7,3 +9,6 @@ echo "Installing basic ZSH plugins"
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "Creating symlink to .zshrc"
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
