53fa15df69bdabddee81ca98c6098864b18a76bc
Gaurav Keswani <gaurav.keswani@shopify.com>
2022-04-12 14:49:53 -0700

    Don't install starship as the default shell

--- setup.sh
+++ setup.sh
@@ -31,6 +31,3 @@ sudo chmod 777 /usr/local/bin/git-cob
 
 echo "Installing exa to replace ls"
 sudo apt-get install -y exa
-
-echo "Installing starship shell"
-sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -f
