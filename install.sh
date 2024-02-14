4b02a4979e7340ba1658e3a78bee6d0414ddd39c
Gaurav Keswani <gauravkeswani92@gmail.com>
2024-02-13 21:44:00 -0800

    Replace setup.sh with install.sh for a more robust setup

---
+++ install.sh
@@ -0,0 +1,53 @@
+#/bin/bash
+
+# The -e option instructs the shell to exit immediately if any command it executes fails 
+# (returns a non-zero status).
+set -e
+
+export DOTFILES_PATH=$HOME/dotfiles
+
+echo "Installing dotfiles"
+if test -d $DOTFILES_PATH; then
+  echo "✅ Dotfiles already installed"
+else
+  echo "✅ Cloning dotfiles"
+  git clone https://github.com/gkeswani92/dotfiles $DOTFILES_PATH
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "✅ Installing Oh My ZSH"
+  sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+echo "✅ Installing Oh My ZSH custom plugins"
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "✅ Install custom git scripts"
+sudo cp -r $DOTFILES_PATH/git/scripts/* /usr/local/bin/
+sudo chmod 777 /usr/local/bin/git-cob
+
+echo "✅ Install local git plugins (like interactive rebase, delta)"
+sudo dpkg -i $DOTFILES_PATH/git/plugins/*.deb
+
+echo "✅ Install remote plugins (git-absorb, gh-copilot, exa)"
+sudo apt-get install -y fzf
+sudo apt-get install -y exa
+sudo apt install git-absorb 
+
+echo "✅ Creating symlinks to dotfiles"
+ln -sf $DOTFILES_PATH/git/.gitconfig ~/.gitconfig
+ln -sf $DOTFILES_PATH/ruby/.pryrc ~/.pryrc
+ln -sf $DOTFILES_PATH/shell/tmux.conf ~/.tmux.conf
+ln -sf $DOTFILES_PATH/shell/.zshrc ~/.zshrc
+ln -sf $DOTFILES_PATH/vim/.vimrc ~/.vimrc
+
+echo "✅Install Github Copilot CLI"
+if command -v gh >/dev/null 2>&1; then
+  gh extension install github/gh-copilot
+else
+  echo "gh is not installed"
+fi
+
+
